5723d3988eb0d7f113b603befb5b6f99bab60960
dishachoraria <95456315+dishachoraria@users.noreply.github.com>
2024-09-14 16:55:30 +0530

    Add files via upload

---
+++ piidetection/server/manager.py
@@ -0,0 +1,40 @@
+import logging
+
+from flask import (
+    Flask,
+)
+
+from .api import add_api_rules, initialize_rules
+
+MANAGE_PREFIX = ""
+CLASSIFY_HOST = "127.0.0.1"
+CLASSIFY_PORT = 10399
+DEBUG = True
+
+SECRET_KEY = "change_this_a_very_unique_secret_key"
+
+
+def run_server(host=CLASSIFY_HOST, port=CLASSIFY_PORT, debug=DEBUG):
+    """Run classification server"""
+
+    app = Flask("piidetection", static_url_path="/assets")
+    app.config["SECRET_KEY"] = SECRET_KEY
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+
+    add_api_rules(app)
+    
+    @app.route('/')
+    def index():
+        return "Welcome to piidetection!"
+
+    if debug:
+        logging.getLogger().addHandler(logging.StreamHandler())
+        logging.basicConfig(
+            filename="piidetection_server.log",
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=logging.DEBUG,
+        )
+
+    initialize_rules()
+
+    app.run(host=host, port=port, debug=debug)
